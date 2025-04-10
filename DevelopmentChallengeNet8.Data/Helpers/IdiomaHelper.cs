ed01f2093e7d2079dbfbdef5cbd70d3aac02dc2c
Rui Moraes <rui-moraes@live.com>
2025-04-10 09:14:19 -0300

    Adiciona testes e classes para formas geométricas
    Implementa novos testes em `DataTestsNew.cs` usando NUnit. Remove referências de projetos antigos e atualiza arquivos `.csproj` para .NET 8.0. Adiciona classes para `Circulo`, `Quadrado`, `Retangulo`, `Trapezio` e `TrianguloEquilatero`, implementando a interface `IFormaGeometrica`. Suporte a localização em inglês, espanhol e italiano para relatórios de formas.

---
+++ DevelopmentChallengeNet8.Data/Helpers/IdiomaHelper.cs
@@ -0,0 +1,23 @@
+﻿using DevelopmentChallengeNet8.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevelopmentChallengeNet8.Data.Helpers
+{
+    public static class IdiomaHelper
+    {
+        public static CultureInfo ObterCulture(int idioma)
+        {
+            switch (idioma)
+            {
+                case (int)Idioma.Espanhol: return new CultureInfo("es");
+                case (int)Idioma.Italiano: return new CultureInfo("it");
+                default: return new CultureInfo("en");
+            }
+        }
+    }
+}
---
+++ DevelopmentChallengeNet8.Data/Interfaces/IFormaGeometrica.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevelopmentChallengeNet8.Data.Interfaces
+{
+    public interface IFormaGeometrica
+    {
+        decimal CalcularArea();
+        decimal CalcularPerimetro();
+        string Nome(int idioma, int quantidade);
+    }
+}
